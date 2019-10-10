a366e6f446630153a4363d1170dcb8985d9791c3
Kaffuccino <kaffudesu@gmail.com>
2019-10-10 07:26:39 -0600

    Updates

---
+++ precache-manifest.699ef1603418a86cc7de358e02607554.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b171b493ccd6817a82234ddb457b936",
+    "url": "/pca-digitall/index.html"
+  },
+  {
+    "revision": "5402e1f399fb2fa1a7d2",
+    "url": "/pca-digitall/static/css/main.bf875527.chunk.css"
+  },
+  {
+    "revision": "b498db7abf69f6c3a8f0",
+    "url": "/pca-digitall/static/js/2.9d77cdb8.chunk.js"
+  },
+  {
+    "revision": "5402e1f399fb2fa1a7d2",
+    "url": "/pca-digitall/static/js/main.2a0b28b6.chunk.js"
+  },
+  {
+    "revision": "248726be0996d0c017f4",
+    "url": "/pca-digitall/static/js/runtime-main.ae394ac5.js"
+  }
+]);
